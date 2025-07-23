2e2f92f168a2f9f8b44247a839bdf9d2c3eda35e
Denys Bushuliak <denys.b@devbrother.com>
2025-07-23 18:56:07 +0000

    fix: formating

--- packages/client/src/entities/Amount.ts
+++ packages/client/src/entities/Amount.ts
@@ -1,18 +1,17 @@
-
 /**
  * Represents an amount of assets in the F1R3Cap system.
  */
 export class Amount {
-    private constructor(private value: number) { }
+  private constructor(private value: number) {}
 
-    public static tryFrom(value: number): Amount {
-        if (value <= 0) {
-            throw new Error("Amount cannot be negative or zero.");
-        }
-        return new Amount(value);
+  public static tryFrom(value: number): Amount {
+    if (value <= 0) {
+      throw new Error("Amount cannot be negative or zero.");
     }
+    return new Amount(value);
+  }
 
-    public getValue(): number {
-        return this.value;
-    }
+  public getValue(): number {
+    return this.value;
+  }
 }
--- packages/client/src/entities/Description.ts
+++ packages/client/src/entities/Description.ts
@@ -4,27 +4,26 @@ const MAX_DESCRIPTION_LENGTH = 100;
  * Description entity for handling string values with a maximum length.
  */
 export class Description {
+  private constructor(private value: string) {}
 
-    private constructor(private value: string) { }
-
-    /**
-     * Creates a new Description instance.
-     * @param value - The description string.
-     * @throws Error if the value exceeds 100 characters.
-     */
-    public static tryFromString(value: string) {
-        if (value.length <= MAX_DESCRIPTION_LENGTH) {
-            return new Description(value)
-        } else {
-            throw new Error("Value greater then 100 characters")
-        }
+  /**
+   * Creates a new Description instance.
+   * @param value - The description string.
+   * @throws Error if the value exceeds 100 characters.
+   */
+  public static tryFromString(value: string) {
+    if (value.length <= MAX_DESCRIPTION_LENGTH) {
+      return new Description(value);
+    } else {
+      throw new Error("Value greater then 100 characters");
     }
+  }
 
-    /**
-     * Gets the value of the description.
-     * @returns The description string.
-     */
-    public getValue() {
-        return this.value;
-    }
+  /**
+   * Gets the value of the description.
+   * @returns The description string.
+   */
+  public getValue() {
+    return this.value;
+  }
 }
