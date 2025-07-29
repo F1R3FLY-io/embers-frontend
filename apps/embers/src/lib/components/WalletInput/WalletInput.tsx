158e1e5ca630a3fb875e034b79b57c440846a351
ihor.r <ihor.r@devbrother.com>
2025-07-29 12:09:37 +0200

    feat: key serialization

--- apps/embers/src/lib/components/WalletInput/WalletInput.tsx
+++ apps/embers/src/lib/components/WalletInput/WalletInput.tsx
@@ -1,5 +1,6 @@
+import { base16 } from "@scure/base";
 import classNames from "classnames";
-import { PrivateKey } from "embers-client-sdk";
+import { deserializeKey, PrivateKey } from "embers-client-sdk";
 import { type ChangeEvent, useCallback, useEffect, useState } from "react";
 
 import { FilePicker } from "@/lib/components/FilePicker";
@@ -24,13 +25,16 @@ export default function WalletInput({ error, onChange }: WalletInputProps) {
 
   const [content, setContent] = useState("");
 
-  const setWallet = useCallback(
-    (content: string) => {
+  const setWalletFromInput = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      e.stopPropagation();
+      e.preventDefault();
+
+      const content = e.target.value;
+
       setContent(content);
       try {
-        onChange({
-          privateKey: PrivateKey.tryFromHex(content),
-        });
+        onChange({ privateKey: PrivateKey.tryFromHex(content) });
         setErrorState(false);
       } catch {
         onChange(undefined);
@@ -40,23 +44,23 @@ export default function WalletInput({ error, onChange }: WalletInputProps) {
     [onChange],
   );
 
-  const setWalletFromInput = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      e.stopPropagation();
-      e.preventDefault();
-      setWallet(e.target.value);
-    },
-    [setWallet],
-  );
-
   const setWalletFromFile = useCallback(
     (file: File) => {
       const reader = new FileReader();
-      reader.onload = () => setWallet(reader.result as string);
+      reader.onload = () => {
+        try {
+          const privateKey = deserializeKey(reader.result as string);
+          onChange({ privateKey });
+          setContent(base16.encode(privateKey.value));
+          setErrorState(false);
+        } catch {
+          setErrorState(true);
+        }
+      };
       reader.onerror = () => setErrorState(true);
       reader.readAsText(file);
     },
-    [setWallet],
+    [onChange],
   );
 
   const inputAreaClass = classNames(styles["input-area"], {
---
+++ packages/client/src/serialization.ts
@@ -0,0 +1,33 @@
+import { base16 } from "@scure/base";
+import z from "zod";
+
+import { PrivateKey } from "./entities/PrivateKey";
+
+export const ValueFormat = z.enum(["hex"]);
+export type ValueFormat = z.infer<typeof ValueFormat>;
+
+export const KeyType = z.enum(["secp256k1"]);
+export type KeyType = z.infer<typeof KeyType>;
+
+export const WalletFile = z.object({
+  keyType: KeyType,
+  value: z.string(),
+  valueFormat: ValueFormat,
+});
+export type WalletFile = z.infer<typeof WalletFile>;
+
+export function serializeKey(key: PrivateKey): string {
+  const fileContent: WalletFile = {
+    keyType: "secp256k1",
+    value: base16.encode(key.value),
+    valueFormat: "hex",
+  };
+
+  return JSON.stringify(fileContent);
+}
+
+export function deserializeKey(fileContent: string): PrivateKey {
+  const json: unknown = JSON.parse(fileContent);
+  const { value } = WalletFile.parse(json);
+  return PrivateKey.tryFromHex(value);
+}
