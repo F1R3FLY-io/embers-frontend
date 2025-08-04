ac9232ffe73fa7fd3de00cda604b771deaddf435
Aidan Stay <aidan@pyrofex.net>
2025-08-04 14:59:23 -0600

    remove text custom hover

--- apps/embers/src/lib/components/Text/Text.module.scss.d.ts
+++ apps/embers/src/lib/components/Text/Text.module.scss.d.ts
@@ -3,6 +3,5 @@ declare const classNames: {
   readonly title: "title";
   readonly secondary: "secondary";
   readonly "no-color": "no-color";
-  readonly "custom-hover": "custom-hover";
 };
 export default classNames;
