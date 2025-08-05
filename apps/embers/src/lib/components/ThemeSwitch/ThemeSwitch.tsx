a82aa3bf173e4e7da73738e4c65ca82dce4fbc4e
aidanstay <aidan@pyrofex.net>
2025-08-05 07:45:58 -0600

    Dashboard page (#7)
    Adds Dashboard page and ThemeSwitch component.

---
+++ apps/embers/src/lib/components/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,50 @@
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+
+import styles from "./ThemeSwitch.module.scss";
+
+const updateTheme = (dark: boolean) => {
+  if (dark) {
+    document.documentElement.classList.remove("light-theme");
+  } else {
+    document.documentElement.classList.add("light-theme");
+  }
+  localStorage.setItem("theme", dark ? "dark" : "light");
+};
+
+interface ThemeSwitchProps {
+  className?: string;
+}
+
+export function ThemeSwitch({ className }: ThemeSwitchProps) {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    // Check for saved theme preference or default to dark
+    const savedTheme = localStorage.getItem("theme") ?? "dark";
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    ).matches;
+    const shouldBeDark = savedTheme === "dark" ? true : prefersDark;
+
+    setIsDarkMode(shouldBeDark);
+    updateTheme(shouldBeDark);
+  }, []);
+
+  const toggleTheme = () => {
+    setIsDarkMode((isDarkMode) => {
+      const newIsDarkMode = !isDarkMode;
+      updateTheme(newIsDarkMode);
+      return newIsDarkMode;
+    });
+  };
+
+  return (
+    <div className={classNames(styles["theme-switch"], className)}>
+      <label className={styles.switch}>
+        <input checked={isDarkMode} type="checkbox" onChange={toggleTheme} />
+        <span className={styles.slider}></span>
+      </label>
+    </div>
+  );
+}
