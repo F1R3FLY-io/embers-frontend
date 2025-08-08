6c8f75735a9020c4645d529fb275aadbde37966c
Aidan Stay <aidan@pyrofex.net>
2025-08-08 01:50:01 -0600

    svgr from agent-teams branch

--- apps/embers/vite.config.ts
+++ apps/embers/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import sassDts from "vite-plugin-sass-dts";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       enabledMode: ["development", "production"],
       esmExport: true,
     }),
+    svgr(),
   ],
   resolve: {
     alias: {
