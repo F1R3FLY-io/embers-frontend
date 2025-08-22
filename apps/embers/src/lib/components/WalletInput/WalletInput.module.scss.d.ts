f611d8af442914ffcd91c88bebae6c056d160dcd
Oleksandr Kyryliuk <oleksandr.kyryliuk@rightinformation.com>
2025-08-22 12:06:31 +0200

    build fix

--- apps/embers/src/lib/components/WalletInput/WalletInput.module.scss.d.ts
+++ apps/embers/src/lib/components/WalletInput/WalletInput.module.scss.d.ts
@@ -1,2 +1,8 @@
-declare const classNames: {};
+declare const classNames: {
+  readonly container: "container";
+  readonly "input-area": "input-area";
+  readonly input: "input";
+  readonly icon: "icon";
+  readonly error: "error";
+};
 export default classNames;
