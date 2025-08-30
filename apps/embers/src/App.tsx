dbcaa918ad5b67977455295fd9a70f71684027c4
Aidan Stay <aidan@pyrofex.net>
2025-08-29 18:13:54 -0600

    progress

--- apps/embers/src/App.tsx
+++ apps/embers/src/App.tsx
@@ -12,11 +12,7 @@ import styles from "./App.module.scss";
 import "./index.scss";
 
 const Dashboard = lazy(async () => import("@/pages/Dashboard"));
-<<<<<<< HEAD
 const Deploy = lazy(async () => import("@/pages/Deploy/Deploy"));
-const Edit = lazy(async () => import("@/pages/Edit"));
-=======
->>>>>>> 2c5b06cc884b9b7995846052701325bf17a28eb4
 const Home = lazy(async () => import("@/pages/Home"));
 const Login = lazy(async () => import("@/pages/Login"));
 const CreateAiTeamFlow = lazy(async () => import("@/pages/CreateAiTeamFlow"));
@@ -29,25 +25,6 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <WalletProvider>
         <ThemeProvider>
-<<<<<<< HEAD
-          <ReactFlowProvider>
-            <BrowserRouter>
-              <div className={styles.background}>
-                <Routes>
-                  <Route element={<Home />} path="/" />
-                  <Route element={<Login />} path="/login" />
-                  {/* until full page is ready */}
-                  <Route element={<GraphEditor />} path="/editor" />
-                  <Route element={<Deploy />} path="/deploy" />
-                  <Route element={<ProtectedRoute />}>
-                    <Route element={<Dashboard />} path="/dashboard" />
-                    <Route element={<Edit />} path="/edit" />
-                  </Route>
-                </Routes>
-              </div>
-            </BrowserRouter>
-          </ReactFlowProvider>
-=======
           <LayoutProvider>
             <ReactFlowProvider>
               <BrowserRouter>
@@ -59,6 +36,7 @@ export default function App() {
                       element={<CreateAiTeamFlow />}
                       path="/create-ai-team"
                     />
+                    <Route element={<Deploy />} path="/deploy" />
                     <Route element={<ProtectedRoute />}>
                       <Route element={<Dashboard />} path="/dashboard" />
                       <Route
@@ -71,7 +49,6 @@ export default function App() {
               </BrowserRouter>
             </ReactFlowProvider>
           </LayoutProvider>
->>>>>>> 2c5b06cc884b9b7995846052701325bf17a28eb4
         </ThemeProvider>
       </WalletProvider>
     </QueryClientProvider>
