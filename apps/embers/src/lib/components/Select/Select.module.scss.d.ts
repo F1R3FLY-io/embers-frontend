8cc46a8aef7eb2bb4a4c5c56b054667351e3058c
OleksandrK-DevBrother <oleksandr.kyr@devbrother.com>
2025-09-01 15:22:18 +0200

    Added custom select component; added i18n translations (#68)
    * Added custom select component; added i18n translations
    
    * Added custom select component; added i18n translations
    
    * translations update
    
    * fixed language select
    
    * fixed language select

---
+++ apps/embers/src/lib/components/Select/Select.module.scss.d.ts
@@ -0,0 +1,17 @@
+declare const classNames: {
+  readonly container: "container";
+  readonly label: "label";
+  readonly select: "select";
+  readonly open: "open";
+  readonly error: "error";
+  readonly disabled: "disabled";
+  readonly borderless: "borderless";
+  readonly value: "value";
+  readonly placeholder: "placeholder";
+  readonly arrow: "arrow";
+  readonly dropdown: "dropdown";
+  readonly option: "option";
+  readonly selected: "selected";
+  readonly tag: "tag";
+};
+export default classNames;
