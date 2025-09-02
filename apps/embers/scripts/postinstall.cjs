fc41f16b50af7cb0cef220558ea8bc60af5d37ee
Dylon Edwards <dylon.devo@gmail.com>
2025-09-02 12:13:16 -0400

    Adds support for :global CSS selector to stylelint; cleans up Lightning Bug SCSS and postinstall script according to the linters' directions

--- apps/embers/scripts/postinstall.cjs
+++ apps/embers/scripts/postinstall.cjs
@@ -1,4 +1,11 @@
-const fs = require('fs');
-fs.mkdirSync('public/js', { recursive: true });
-fs.cpSync('node_modules/@f1r3fly-io/lightning-bug/resources/public/extensions', 'public/extensions', { recursive: true });
-fs.cpSync('node_modules/@f1r3fly-io/lightning-bug/resources/public/js/tree-sitter.wasm', 'public/js/tree-sitter.wasm');
+const fs = require("fs");
+fs.mkdirSync("public/js", { recursive: true });
+fs.cpSync(
+  "node_modules/@f1r3fly-io/lightning-bug/resources/public/extensions",
+  "public/extensions",
+  { recursive: true },
+);
+fs.cpSync(
+  "node_modules/@f1r3fly-io/lightning-bug/resources/public/js/tree-sitter.wasm",
+  "public/js/tree-sitter.wasm",
+);
