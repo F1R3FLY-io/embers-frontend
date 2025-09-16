3de75404fa432bf2390a9b7a216b04f72ac22039
OleksandrK-DevBrother <oleksandr.kyr@devbrother.com>
2025-09-16 11:35:17 +0200

    Additional components (#89)
    * Additional components
    
    * Additional components
    
    * Additional components
    
    * Additional components

---
+++ apps/embers/src/lib/components/RadioPills/RadioPills.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly group: "group";
+  readonly pill: "pill";
+  readonly body: "body";
+  readonly bordered: "bordered";
+  readonly active: "active";
+};
+export default classNames;
