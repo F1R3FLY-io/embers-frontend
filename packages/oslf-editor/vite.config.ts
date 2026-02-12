f362795518e154db4f05151bc70a4b6c4cf1e0ec
IhorR-DevBrother <ihor.r@devbrother.com>
2026-02-12 16:43:27 +0100

    fix docker (#186)
    * fix docker
    
    * fix

--- packages/oslf-editor/vite.config.ts
+++ packages/oslf-editor/vite.config.ts
@@ -9,32 +9,6 @@ export default defineConfig({
       fileName: "index",
       formats: ["es"],
     },
-    outDir: "dist",
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "blockly/core",
-        "blockly/msg/en",
-        "@blockly/plugin-cross-tab-copy-paste",
-      ],
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.names.includes("style.css")) {
-            return "styles/blockly-overrides.css";
-          }
-          return assetInfo.names[0] ?? "assets/[name].[ext]";
-        },
-        globals: {
-          "@blockly/plugin-cross-tab-copy-paste": "BlocklyCrossTabCopyPaste",
-          "blockly/core": "Blockly",
-          "blockly/msg/en": "BlocklyMsgEn",
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-        preserveModules: false,
-      },
-    },
     sourcemap: true,
   },
   plugins: [
